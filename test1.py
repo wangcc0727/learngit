7c69c9da440acaefbf4979f239a9bb7e5f6dd022
root <root@wangchentao.lx.dev.bj.leonge.com>
2024-07-31 13:47:43 +0800

    add test file

---
+++ test1.py
@@ -0,0 +1,10 @@
+import sys
+import os
+import stat
+
+print("len", len(sys.argv))
+print("0", sys.argv[0])
+print("1", sys.argv[1])
+print("2", sys.argv[2])
+print("3", sys.argv[3])
+print("4", sys.argv[4])
